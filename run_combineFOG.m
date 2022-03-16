273d5a78a4d451218cc8304eddc93f3b0747da69
helenacockx <h.cockx@donders.ru.nl>
2022-03-16 16:49:10 +0100

    Update run_combineFOG.m

--- run_combineFOG.m
+++ run_combineFOG.m
@@ -1,18 +1,16 @@
 %% set-up:
 clear all; close all;
+addpath C:\Users\helen\Documents\PhD\FOG_annotation\matlabscripts\FOG_annotations
 
 folder_rater1='\\dcn-srv.science.ru.nl\dcn\biophysics\prompt\freezing_fnirs\data\processed\annotations\Helena';
 folder_rater2='\\dcn-srv.science.ru.nl\dcn\biophysics\prompt\freezing_fnirs\data\processed\annotations\Yuli';
 folder_combined='\\dcn-srv.science.ru.nl\dcn\biophysics\prompt\freezing_fnirs\data\processed\annotations\combined';
 
-subjects={'PD10', 'PD11'};
+% subjects={'PD62', 'PD61', 'PD90', 'PD77', 'PD35', 'PD46', 'PD50'};
+% subjects={'PD10', 'PD11', 'PD15', 'PD22', 'PD31', 'PD62', 'PD61', 'PD90', 'PD77', 'PD35', 'PD46', 'PD50'};
+subjects = {'PD06'};
 
-sf=1000; % choose sampling frequency
-ts=(1/sf); % time steps 
-
-tolerance_sec=inf;    % Tolerance in sec (if inf --> always include/exclude when overlappping annotations)
-tolerance=tolerance_sec*sf;
-
+tolerance_sec=2;    % Tolerance in sec (if inf --> always include/exclude when overlappping annotations)
 correction='include'; % or exclude
 
 calculate_agreement=true; % this only makes sense if running the script for multiple subjects
@@ -25,9 +23,11 @@ for s=1:length(subjects)
   % check if the same number of files are found for the two annotators
   if length(files{1})~= length(files{2})
     error('Not the same number of annotation files (.txt) found in the two annotator folders for subject %s',subjects{s}) 
+  elseif isempty(files{1}) | isempty(files{2})
+    error('No annotation files (.txt) found for subject %s', subjects{s})
   else
     % sort names in ascending order so order of files for the two rater
-    % corresond (make sure they have similar names though!)
+    % correspond (make sure they have similar names though!)
     for i=1:2
       [~, idx]=sort({files{i}.name});
       files{i}=files{i}(idx);
@@ -38,7 +38,7 @@ for s=1:length(subjects)
   for f=1:length(files{1})
     file_rater1=fullfile(files{1}(f).folder, files{1}(f).name);
     file_rater2=fullfile(files{2}(f).folder, files{2}(f).name);
-    file_combined=fullfile(folder_combined, sprintf('sub-%s_file-%02d.tsv', subjects{s}, f));
+    file_combined=fullfile(folder_combined, sprintf('sub-%s_annotations-combined.tsv', subjects{s}));
     agreement_table=fullfile(folder_combined, 'agreement_table.tsv');
     combine_FOGannotations(file_rater1, file_rater2, file_combined, agreement_table, subjects{s}, 'include', 2)
   end
